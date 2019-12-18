bfec526283e9c4e2e5cffd39dfd85e1576cfa9a0
Vinh-Mac <vinh@abivin.com>
2019-12-18 16:22:27 +0700

    Create src folder with basic files + add redux, redux-react

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore } from 'redux'
+import calculator from './src/reducer';
+import { Provider } from 'react-redux';
+import CalculatorScreen from './src/Screen';
+import styles from './src/styles';
 
+let store = createStore(calculator)
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hi Vinh, I am expo project</Text>
+    <View style={styles.screenContainer}>
+      <Provider store={store}>
+        <CalculatorScreen />
+      </Provider>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/Screen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from "react-native"
+import styles from './styles';
+const CalculatorScreen = (props) => {
+    return <View style={styles.screenContainer}>
+        <Text>Hi Vinh, I am Calcualtor screen</Text>
+    </View>
+}
+
+export default CalculatorScreen
