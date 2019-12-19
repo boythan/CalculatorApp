8d27047218c82a47180b70d432c4e71c4a0728ec
Vinh-Mac <vinh@abivin.com>
2019-12-19 11:47:34 +0700

    refactor reducer

--- src/reducer.js
+++ src/reducer.js
@@ -6,81 +6,39 @@ const initialState = {
 }
 function calculator(state = initialState, button) {
     const { lastButtonClick, operatorString, resultToDisplay } = state
-
     switch (button.type) {
         case constants.BUTTON_TYPE.NUMBER:
-            if (!lastButtonClick) {
-                return {
-                    lastButtonClick: button,
-                    resultToDisplay: button.content,
-                    operatorString: button.content
-                }
-            } else if (lastButtonClick.type === constants.BUTTON_TYPE.OPERATOR) {
-                return {
-                    lastButtonClick: button,
-                    resultToDisplay: button.content,
-                    operatorString: operatorString + button.content
-
-                }
-            } else if (lastButtonClick.type === constants.BUTTON_TYPE.RESULTING) {
-                return {
-                    lastButtonClick: button,
-                    resultToDisplay: button.content,
-                    operatorString: button.content
-
-                }
-            } else {
-                return {
-                    lastButtonClick: button,
-                    resultToDisplay: resultToDisplay + button.content,
-                    operatorString: operatorString + button.content
-                }
+            let resultToDis = operatorStr = button.content;
+            if (lastButtonClick && lastButtonClick.type !== constants.BUTTON_TYPE.OPERATOR && lastButtonClick.type !== constants.BUTTON_TYPE.RESULTING) {
+                resultToDis = resultToDisplay + button.content
             }
+            if (lastButtonClick && lastButtonClick.type !== constants.BUTTON_TYPE.RESULTING) {
+                operatorStr = operatorString + button.content
+            }
+            return { lastButtonClick: button, resultToDisplay: resultToDis, operatorString: operatorStr }
         case constants.BUTTON_TYPE.OPERATOR:
             if (!lastButtonClick) {
                 return initialState;
             } else if (lastButtonClick.type === constants.BUTTON_TYPE.OPERATOR) {
-                return {
-                    lastButtonClick: button,
-                    resultToDisplay: resultToDisplay,
-                    operatorString: operatorString.slice(0, -1) + button.content
-                }
+                return { ...state, lastButtonClick: button, operatorString: operatorString.slice(0, -1) + button.content }
+            } else {
+                return { ...state, lastButtonClick: button, operatorString: operatorString + button.content }
             }
-            return {
-                lastButtonClick: button,
-                resultToDisplay: resultToDisplay,
-                operatorString: operatorString + button.content
-            }
-
         case constants.BUTTON_TYPE.RESULTING:
             try {
                 const result = eval(operatorString)
                 if (result === constants.INFINITY_NUMBER) {
-                    return {
-                        lastButtonClick: null,
-                        resultToDisplay: result,
-                        operatorString: ''
-                    }
+                    return { lastButtonClick: null, resultToDisplay: result, operatorString: '' }
                 }
-                return {
-                    lastButtonClick: button,
-                    resultToDisplay: result,
-                    operatorString: result
-                }
+                return { lastButtonClick: button, resultToDisplay: result, operatorString: result }
             } catch (error) {
                 alert(error.message)
             }
             return initialState
-
-
         case constants.BUTTON_TYPE.CLEAR:
             return initialState
         default:
-            return {
-                resultToDisplay: resultToDisplay,
-                operatorString: operatorString
-            }
+            return initialState
     }
 }
-
 export default calculator
